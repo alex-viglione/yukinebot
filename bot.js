2edb338d98aeee03ebff1890140d49a4f6b7784b
Alexandre Viglione <alexxe33@gmail.com>
2019-12-25 21:00:29 +0100

    new feature

--- bot.js
+++ bot.js
@@ -1,5 +1,3 @@
-console.log('Hey I\'m here');
-
 const Twit = require('twit');
 const fs = require('fs');
 const path = require('path');
@@ -18,17 +16,22 @@ const T = new Twit(config);
 const random_from_array = images => images[Math.floor(Math.random() * images.length)];
 
 
-const upload_random_image = images => {
+const upload_random_image = (images, statusText) => {
+    console.log('Fetching a random image');
     const image_path = path.join(__dirname, '/images/' + random_from_array(images));
+
+    console.log('Encoding the image');
     const b64content = fs.readFileSync(image_path, { encoding: 'base64' });
 
+    console.log('Uploading the image as media on twitter');
     T.post('media/upload', { media_data: b64content }, (err, data, response) => {
         if (err) {
             console.log('ERROR:');
             console.log(err);
         } else {
+            console.log('Posting that media with a status');
             T.post('statuses/update', {
-                status: 'Daily Yukine',
+                status: statusText,
                 media_ids: new Array(data.media_id_string)
             },
                 (err, data, response) => {
@@ -44,17 +47,39 @@ const upload_random_image = images => {
     });
 };
 
-fs.readdir(__dirname + '/images', (err, files) => {
-    if (err) {
-        console.log(err);
-    } else {
-        const images = [];
-        files.forEach(f => {
-            images.push(f);
-        });
+const loadImages = () => {
+    fs.readdir(__dirname + '/images', (err, files) => {
+        if (err) {
+            console.log(err);
+        } else {
+            const images = [];
+            files.forEach(f => {
+                images.push(f);
+            });
+            return images;
+        }
+    });
+}
 
-        setInterval(() => {
-            upload_random_image(images);
-        }, 86400000);
+const imagesArray = loadImages();
+
+setInterval(() => {
+    upload_random_image(imagesArray, 'Daily Yukine');
+}, 82800000);
+
+const stream = T.stream('user');
+stream.on('tweet', replyZenbu);
+
+
+const replyZenbu = msgContent => {
+    const replyto = msgContent.in_reply_to_screen_name;
+    const text = msgContent.text.toLowerCase();
+    const from = msgContent.user.screen_name;
+
+    console.log(replyto + ' ' + from);
+
+    if (replyto === 'yukinebotine' && text.includes('post chris')) {
+        const statusText = `@${from} Zenbu!`;
+        upload_random_image(imagesArray, statusText);
     }
-});
+}
