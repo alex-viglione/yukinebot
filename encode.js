75aea79dc9395bcb6ae881948bf412432c92eb3c
Alex Viglione <alexxe33@gmail.com>
2020-04-10 16:25:57 +0200

    Added axios

--- encode.js
+++ encode.js
@@ -1,1 +1,8 @@
-require('dotenv').config()
+require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
+const axios = require('axios');
+
+const postData = (img64) => {
+
+}
