5fef7b0340df25782b6b297ca21778f8babda915
Alex Viglione <alexxe33@gmail.com>
2020-07-27 15:12:09 +0200

    Restructured program a bit

--- bot.js
+++ bot.js
@@ -16,13 +16,54 @@ const Twit = require('twit');
 const config = require('./config');
 const T = new Twit(config);
 
+
+function postPic(picData) {
+    T.post('media/upload', { media_data: picData }, (err, data, response) => {
+        if (err) {
+            console.log(`Error: ${err}`);
+        } else {
+            T.post('statuses/update', {
+                status: '<TESTING IN PROGRESS>',
+                media_ids: new Array(data.media_id_string)
+            },
+                (err, data, response) => {
+                    if (err) {
+                        console.log(`Error: ${err}`);
+                    } else {
+                        console.log('--- Posted an image! Now exiting... ---');
+                        process.exit(0);
+                    }
+                }
+            );
+        }
+    });
+}
+
+function encode(path) {
+    const b64content = fs.readFileSync(path, { encoding: 'base64' });
+    return b64content;
+}
+
+function fetchPic(read, write, choice) {
+    read.pipe(write);
+
+    write.on('close', function () {
+        const image_path = path.join(__dirname, '/fromdb/' + choice);
+        const b64 = encode(image_path);
+        
+        postPic(b64);
+    });
+}
+
 function upload_random_image() {
+
     conn.once('open', function () {
+
         const gfs = new mongoose.mongo.GridFSBucket(conn.db);
         mongoose.model('filename', { filename: String }, 'fs.files');
         const filename = mongoose.model('filename');
-
         filename.find({}, (err, data) => {
+
             if (err) {
                 console.log('Error: ', err);
             }
@@ -30,43 +71,15 @@ function upload_random_image() {
             data.forEach(el => {
                 namesArray.push(el.filename);
             });
-
-            setInterval(() => {
-
-                const choice = namesArray[Math.floor(Math.random() * namesArray.length)];
-
-                const readStream = gfs.openDownloadStreamByName(choice);
-                const fs_write_stream = fs.createWriteStream(path.join(__dirname, `/fromdb/${choice}`));
-                readStream.pipe(fs_write_stream);
-
-                fs_write_stream.on('close', function () {
-                    const image_path = path.join(__dirname, '/fromdb/' + choice);
-                    const b64content = fs.readFileSync(image_path, { encoding: 'base64' });
-
-                    T.post('media/upload', { media_data: b64content }, (err, data, response) => {
-                        if (err) {
-                            console.log(`Error: ${err}`);
-                        } else {
-                            T.post('statuses/update', {
-                                status: 'Daily Yukine',
-                                media_ids: new Array(data.media_id_string)
-                            },
-                                (err, data, response) => {
-                                    if (err) {
-                                        console.log(`Error: ${err}`);
-                                    } else {
-                                        console.log('Posted an image!');
-                                    }
-                                }
-                            );
-                        }
-                    });
-
-                });
-            }, 43200000);
+            const choice = namesArray[Math.floor(Math.random() * namesArray.length)];
+            const readStream = gfs.openDownloadStreamByName(choice);
+            const fs_write_stream = fs.createWriteStream(path.join(__dirname, `/fromdb/${choice}`));
+            fetchPic(readStream, fs_write_stream, choice);
 
         });
+
     });
+
 }
 
 upload_random_image();
